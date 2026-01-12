b084657e0897f8c917ee9f6b083d803f896912da
Rabib Haque <rabibhaque200@gmail.com>
2026-01-12 15:41:21 +0600

    Initializing Database and Docker

--- backend/todo-backend/src/database/database.module.ts
+++ backend/todo-backend/src/database/database.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<'mysql'>('DATABASE_TYPE') || 'mysql',
+        host: configService.get<string>('DATABASE_HOST') || 'localhost',
+        port: configService.get<number>('DATABASE_PORT') || 3306,
+        username: configService.get<string>('DATABASE_USERNAME') || '',
+        password: configService.get<string>('DATABASE_PASSWORD') || '',
+        database: configService.get<string>('DATABASE_NAME') || '',
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
